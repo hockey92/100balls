786e41abe27235f1ab52d9ab36b0e3387089e914
hockey92 <shibachel@gmail.com>
2016-02-13 15:19:17 +0500

    qeew

--- app/src/main/jni/Collision.cpp
+++
@@ -1,8 +0,0 @@
-#include "Collision.h"
-
-void Collision::swap() {
-    normal *= -1;
-    Vec2 c = r1;
-    r1 = r2;
-    r2 = c;
-}
--- app/src/main/jni/ImpulseFactory.cpp
+++
@@ -1,9 +0,0 @@
-#include "ImpulseFactory.h"
-#include "Constants.h"
-
-float ImpulseFactory::createImpulse(const PhysicsObject *o1, const PhysicsObject *o2,
-                                    const Collision *c) {
-    float vrn = Vec2::dot(o2->getVel() - o1->getVel(), c->getNormal());
-    vrn -= c->getDist() / DT;
-    return -1.f * vrn / (o1->getInvM() + o2->getInvM());
-}
